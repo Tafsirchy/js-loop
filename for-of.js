af91c03e3261f904eb99d0b5cc229fc89b6e044b
Tafsirchy <tafsirchy1000@gmail.com>
2025-07-29 22:36:21 +0600

    for of

---
+++ for-of.js
@@ -0,0 +1,19 @@
+const numbers = [12, 98, 45, 63, 21, 72, 52, 81, 19];
+
+console.log(numbers);
+
+for (const number of numbers){
+    console.log(number);
+}
+
+
+const fruits = ['apple', 'banana', 'mango', 'orange', 'kiwi'];
+
+// for(){
+
+// }
+
+for(const fruit of fruits){
+    console.log(fruit);
+    console.log("I want to eat")
+}
