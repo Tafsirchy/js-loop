61cebe58a8375f4b7142ab66381f75f97f09e958
Tafsirchy <tafsirchy1000@gmail.com>
2025-07-29 22:53:31 +0600

    while

---
+++ while.js
@@ -0,0 +1,19 @@
+const number = 7;
+
+if(number < 10){
+    console.log('choto ekta number paichi');
+}
+console.log("kono number paini");
+
+// while loop
+// while(condition){
+//     //body of loop
+// }
+
+let num=0; //loop variable
+while(num < 5){
+    console.log("looooooooop", num);
+    // num = num + 1; // num++
+    // num += 1; // num++
+    num++;
+}
