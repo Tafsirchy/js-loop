beffc77e74179b3944aa81d2daf2ecddc7a19791
Tafsirchy <tafsirchy1000@gmail.com>
2025-07-29 23:45:03 +0600

    range

---
+++ range.js
@@ -0,0 +1,34 @@
+// let num = 1;
+
+// while(num <= 10){ //focus on the condition
+//     console.log(num);
+//     num++;
+// }
+
+// let num = 50;
+
+// while(num <= 100){
+//     console.log(num);
+//     num++;
+// }
+
+// let num = 1;
+// let sum = 0;
+
+// while(num <= 10){
+    
+//     console.log(num);
+//     sum = sum + num;
+//     console.log(sum);
+//     num++;
+// }
+
+let num = 2;
+
+while(num <= 10){
+    console.log(num);
+    if(num % 2 == 0){
+        console.log('even', num);
+    }
+    num ++;
+}
