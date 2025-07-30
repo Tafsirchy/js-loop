79ec5220b218b2a10b5640b2450b9bf97ca5b82a
Tafsirchy <tafsirchy1000@gmail.com>
2025-07-30 11:14:59 +0600

    for-sum

---
+++ for-sum.js
@@ -0,0 +1,19 @@
+//  let sum = 0;
+//  for( let i = 11; i <=20; i++){
+//     sum += i;
+//     console.log(i);
+
+//  }
+
+//  console.log("Sum of number is: ",sum);
+
+// incremental loop
+// for( let i = 1; i <= 10; i++){
+//     console.log(i);
+// }
+
+
+// decremental loop
+for( let i=20; i >= 11; i--){
+    console.log(i);
+}
