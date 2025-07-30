540befdb78e5609984bbd3f0c8168677d77a5762
Tafsirchy <tafsirchy1000@gmail.com>
2025-07-30 12:15:50 +0600

    break-continue

---
+++ break-continue.js
@@ -0,0 +1,36 @@
+// for(let i = 0; i <= 15; i++){
+//     console.log(i);
+//     if(i >= 10){
+//         break;
+//     }
+// }
+
+
+// console.log('life after breakup');
+
+// let i = 54;
+// while(i >= 25){
+    
+//     if(i < 50){
+//         break;
+//     }
+//     console.log(i);
+//     i--;
+// }
+
+// for(let i = 0; i <= 10; i++){
+//     if(i % 2 !== 0){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+let n = 0;
+while(n < 15){
+    n++;
+    if(n % 5 !== 0){
+        continue;
+        
+    }
+    console.log(n);
+}
