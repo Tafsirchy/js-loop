d402f77430d85d096b07892b884fe604fe8100b2
Tafsirchy <tafsirchy1000@gmail.com>
2025-07-30 11:23:09 +0600

    while-sum

---
+++ while-sum.js
@@ -0,0 +1,14 @@
+// let sum = 0;
+// let i = 0;
+// while(i <= 10){
+//     sum += i;
+//     console.log(i);
+//     i++;
+// }
+// console.log("Sum of number is: ",sum);
+
+let i = 10;
+while(i >= 0){
+    console.log(i);
+    i--;
+}
