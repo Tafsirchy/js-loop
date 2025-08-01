2972c2fd9bcb3f9753d64d2c0fa7f36504f0e838
Tafsirchy <tafsirchy1000@gmail.com>
2025-08-01 21:58:07 +0600

    task8

--- task8.js
+++ task8.js
@@ -1,6 +1,14 @@
 let sum = 0;
-for(i=91; i<=129; i++){
-    if(i%2 !== 0){
+// for(i=91; i<=129; i++){
+//     if(i%2 !== 0){
+//         sum = sum + i;
+//         // console.log(i,":",sum);
+//         // console.log(sum);
+//     }
+// }
+
+for(i=51; i<=85; i++){
+    if(i%2 == 0){
         sum = sum + i;
         console.log(i,":",sum);
         // console.log(sum);
