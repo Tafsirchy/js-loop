ba849f0717d485f4f47ab43227be073ac23bbd2c
Tafsirchy <tafsirchy1000@gmail.com>
2025-08-01 22:20:23 +0600

    task12

--- task12.js
+++ task12.js
@@ -1,6 +1,14 @@
-let i=61;
+// let i=61;
+// // while(i<=100){
+// //     if(i%2 !== 0){
+// //         console.log(i);
+// //     }
+// //     i++;
+// // }
+
+let i = 78;
 while(i<=100){
-    if(i%2 !== 0){
+    if(i%2 == 0){
         console.log(i);
     }
     i++;
