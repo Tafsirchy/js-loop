f37c3df7da69db0aec970a6ed8d98f372d2237f8
Tafsirchy <tafsirchy1000@gmail.com>
2025-08-01 22:24:58 +0600

    task13

---
+++ task13.js
@@ -0,0 +1,19 @@
+// let i = 81;
+// let sum = 0;
+// while(i<=131){
+//     if(i%2 !== 0){
+//         sum += i;
+//         console.log(i,' = ' ,sum);
+//     }
+//     i++;
+// }
+
+let i = 206;
+let sum = 0;
+while(i<=311){
+    if(i%2 == 0){
+        sum += i;
+        console.log(i,' = ' ,sum);
+    }
+    i++;
+}
